81695951fe81b3ccb6472a4722f5c21ea5c2e40c
IrisQY <36379302+IrisQY@users.noreply.github.com>
2025-04-03 19:22:24 -0400

    BaseLLM result

---
+++ compile.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import glob
+import json
+
+
+def collect_test():
+    out_data = []
+    for filename in glob.glob("../test/plos/*.json"):
+        with open(filename) as json_file:
+            tmp = json.load(json_file)
+            out_data.append(pd.DataFrame(tmp))
+
+    if out_data:
+        out_df = pd.concat(out_data, ignore_index=True)
+        out_df.to_csv('../test/plosBaseLLM.csv', index=False)
+    else:
+        print("No JSON files found or data is empty.")
+
+
+if __name__ == '__main__':
+    collect_test()
